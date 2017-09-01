1bc790f6c6ddb72c1da2d94ba199f9cd84f5623c
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2017-09-01 12:31:22 +1200

    Prob2 C# Start

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Comp5202_Problem2_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //Start the program with Clear();
+         Console.Clear();
+         
+         
+         //End the program with blank line and instructions
+         Console.ResetColor();
+         Console.WriteLine();
+         Console.WriteLine("Press <Enter> to quit the program");
+         Console.ReadKey();
+        }
+    }
+}
